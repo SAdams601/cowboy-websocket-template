b47703a8406d42930116db18b59021416819be40
Stephen A <StephenAdams.Minn@gmail.com>
2015-01-25 13:18:20 +0000

    Deleted test directory.

--- tests/fiveNodeLocalStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./start.sh node2@127.0.0.1 -detached
-./start.sh node5@127.0.0.1 -detached
-./start.sh node4@127.0.0.1 -detached
-./start.sh node3@127.0.0.1 -detached
-./start.sh node1@127.0.0.1
--- tests/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-erl -name $1 $2 -pa ../ebin -config s_group.config 
-
- 
